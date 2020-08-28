aff0e1a03b5f6d354cc048d782ed080ce1f5a8db
Gabriel <pascoal.gabriel@gmail.com>
2020-08-28 01:25:24 +0000

    feat: adding s3 values on code :p

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-1dvp-12345"
+    bucket = "hackathon-fiap-2dvp-336511"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
